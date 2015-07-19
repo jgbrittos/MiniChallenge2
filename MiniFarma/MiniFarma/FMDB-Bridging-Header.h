07cf078009b0e86f13689e2f5b76499f7235bcb6
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-07-19 11:48:49 -0300

    configurando bridge e adicionando fmdb no projeto

---
+++ MiniFarma/MiniFarma/FMDB-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  FMDB-Bridging-Header.h
+//  MiniFarma
+//
+//  Created by João Gabriel de Britto e Silva on 19/07/15.
+//  Copyright (c) 2015 JGBrittoS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FMDatabase.h"
+
+@interface FMDB_Bridging_Header : UIViewController
+
+@end
