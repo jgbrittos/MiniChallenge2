3ea1ea0eaf59ebe8dd9a1fa9bedf9646874ba638
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-07-19 16:43:12 -0300

    criando modelos vazias de remedios e alertas

---
+++ MiniFarma/MiniFarma/Remedio.swift
@@ -0,0 +1,13 @@
+//
+//  Remedio.swift
+//  MiniFarma
+//
+//  Created by João Gabriel de Britto e Silva on 19/07/15.
+//  Copyright (c) 2015 JGBrittoS. All rights reserved.
+//
+
+import UIKit
+
+class Remedio: NSObject {
+   
+}
