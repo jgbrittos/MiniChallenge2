a74b71618bd571415d631fb342aa51a1e07ad426
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-07-19 11:40:07 -0300

    concluindo inicio de internacionalizacao e acessibilidade

--- MiniFarma/MiniFarma/ViewController.swift
+++ MiniFarma/MiniFarma/ViewController.swift
@@ -11,12 +11,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var labelTeste: UILabel!
+    @IBOutlet weak var labelTesteStoryboard: UILabel!
     @IBOutlet weak var textFieldTeste: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.labelTeste.text = "Testando"
+        self.labelTeste.text = NSLocalizedString("LABELTESTE", comment: "Esse é o label de teste")
+        self.labelTeste.accessibilityLabel = NSLocalizedString("LABELTESTE_ACESSIBILIDADE_LABEL", comment: "Esse é o label de teste um")
+        self.labelTeste.accessibilityHint = NSLocalizedString("LABELTESTE_ACESSIBILIDADE_HINT", comment: "Esse é o label de teste dois")
+        
+        //No caso de text fields o Voice Over lê:
+        // o LABEL
+        // o Place holder
+        // fala que é campo de texto
+        // o HINT
+        self.textFieldTeste.placeholder = NSLocalizedString("TEXTFIELDTESTE", comment: "Esse é o textfield de teste")
+        self.textFieldTeste.accessibilityLabel = NSLocalizedString("TEXTFIELDTESTE_ACESSIBILIDADE_LABEL", comment: "Esse é o label de teste um")
+        self.textFieldTeste.accessibilityHint = NSLocalizedString("TEXTFIELDTESTE_ACESSIBILIDADE_HINT", comment: "Esse é o label de teste dois")
+        
     }
 
     override func didReceiveMemoryWarning() {
