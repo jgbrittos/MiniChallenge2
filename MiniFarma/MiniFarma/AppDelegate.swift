f0077078769472cf7b4b74a9b02cc2b0dc6d8775
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-07-19 10:59:06 -0300

    criando o projeto de novo

--- MiniFarma/MiniFarma/AppDelegate.swift
+++ MiniFarma/MiniFarma/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  MiniFarma
 //
 //  Created by João Gabriel de Britto e Silva on 19/07/15.
-//  Copyright (c) 2015 João Gabriel de Britto e Silva. All rights reserved.
+//  Copyright (c) 2015 JGBrittoS. All rights reserved.
 //
 
 import UIKit
--- MiniFarma/MiniFarma/ViewController.swift
+++ MiniFarma/MiniFarma/ViewController.swift
@@ -3,7 +3,7 @@
 //  MiniFarma
 //
 //  Created by João Gabriel de Britto e Silva on 19/07/15.
-//  Copyright (c) 2015 João Gabriel de Britto e Silva. All rights reserved.
+//  Copyright (c) 2015 JGBrittoS. All rights reserved.
 //
 
 import UIKit
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
         self.labelTeste.text = "Testando"
     }
 
@@ -23,16 +22,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
