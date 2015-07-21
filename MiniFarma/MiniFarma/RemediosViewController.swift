d735886de41e1e3aff668e1ab838aa116dd51e58
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-07-21 14:28:41 -0300

    Fazendo animacao dos botoes de opcoes

--- MiniFarma/MiniFarma/RemediosViewController.swift
+++ MiniFarma/MiniFarma/RemediosViewController.swift
@@ -17,6 +17,15 @@ class RemediosViewController: UIViewController, UITableViewDelegate, UITableView
     let remediosVencidos = ["Pasallix","Viagra"]
     var dadosDaVez = Array<String>()
     
+    let botaoAdicionaFarmacia = UIButton()
+    let botaoAdicionaRemedio = UIButton()
+    let botaoAdicionaAlerta = UIButton()
+    
+    let tamanhoPadraoBotao = CGRectMake(0, 0, 50, 50)
+    let centroInicialPadrao = CGPointMake(UIScreen.mainScreen().bounds.width/2.0, UIScreen.mainScreen().bounds.height-49)//NUMERO MAGICO 49 = ALTURA DA TAB BAR
+    
+    var botoesNaoEstaoVisiveis: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,6 +33,8 @@ class RemediosViewController: UIViewController, UITableViewDelegate, UITableView
         self.tableViewRemedios.dataSource = self
         
         self.dadosDaVez = self.remediosValidos
+        
+        self.criaBotoesDeOcoes()
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -42,7 +53,9 @@ class RemediosViewController: UIViewController, UITableViewDelegate, UITableView
         let buttonImage = UIImage(named: "logo_azul.png")
         let buttonImageVer = UIImage(named: "logo_vermelho.png")
         button.autoresizingMask = UIViewAutoresizing.FlexibleRightMargin | UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleBottomMargin | UIViewAutoresizing.FlexibleTopMargin
-        button.frame = CGRectMake(0.0, 0.0, 50, 50)
+        button.frame = CGRectMake(0.0, 0.0, 60, 60)
+        
+        button.addTarget(self, action: Selector("animacao:"), forControlEvents: UIControlEvents.TouchUpInside)
         
         button.setBackgroundImage(buttonImage, forState:UIControlState.Highlighted)
         button.setBackgroundImage(buttonImageVer, forState:UIControlState.Normal)
@@ -54,6 +67,62 @@ class RemediosViewController: UIViewController, UITableViewDelegate, UITableView
         self.segmentedControlValidadeRemedios.setTitle(NSLocalizedString("SEGMENTEDCONTROLREMEDIOVALIDO", comment: "Remédio válido"), forSegmentAtIndex: 0)
         self.segmentedControlValidadeRemedios.setTitle(NSLocalizedString("SEGMENTEDCONTROLREMEDIOINVALIDO", comment: "Remédio inválido"), forSegmentAtIndex: 1)
         
+    }
+    
+    func criaBotoesDeOcoes(){
+        
+        let buttonImage = UIImage(named: "logo_azul.png")
+        let buttonImageVer = UIImage(named: "logo_vermelho.png")
+        
+        botaoAdicionaFarmacia.frame = tamanhoPadraoBotao
+        botaoAdicionaFarmacia.center = centroInicialPadrao
+        botaoAdicionaFarmacia.setBackgroundImage(buttonImageVer, forState:UIControlState.Normal)
+        botaoAdicionaFarmacia.setBackgroundImage(buttonImage, forState:UIControlState.Highlighted)
+        
+        botaoAdicionaRemedio.frame = tamanhoPadraoBotao
+        botaoAdicionaRemedio.center = centroInicialPadrao
+        botaoAdicionaRemedio.setBackgroundImage(buttonImageVer, forState:UIControlState.Normal)
+        botaoAdicionaRemedio.setBackgroundImage(buttonImage, forState:UIControlState.Highlighted)
+        
+        botaoAdicionaAlerta.frame = tamanhoPadraoBotao
+        botaoAdicionaAlerta.center = centroInicialPadrao
+        botaoAdicionaAlerta.setBackgroundImage(buttonImageVer, forState:UIControlState.Normal)
+        botaoAdicionaAlerta.setBackgroundImage(buttonImage, forState:UIControlState.Highlighted)
+    }
+    
+    func animacao(sender: AnyObject){
+        
+        if botoesNaoEstaoVisiveis {
+            UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveEaseOut , animations: {
+                self.botoesNaoEstaoVisiveis = false
+                
+                self.view.addSubview(self.botaoAdicionaFarmacia)
+                self.view.addSubview(self.botaoAdicionaRemedio)
+                self.view.addSubview(self.botaoAdicionaAlerta)
+                
+                self.botaoAdicionaFarmacia.center.x -= 100
+                self.botaoAdicionaFarmacia.center.y -= 100
+                
+                self.botaoAdicionaRemedio.center.y -= 150
+                
+                self.botaoAdicionaAlerta.center.x += 100
+                self.botaoAdicionaAlerta.center.y -= 100
+                
+                }, completion: nil)
+        }else{
+            UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveEaseOut , animations: {
+                self.botoesNaoEstaoVisiveis = true
+                
+                self.botaoAdicionaFarmacia.center = self.centroInicialPadrao
+                self.botaoAdicionaRemedio.center = self.centroInicialPadrao
+                self.botaoAdicionaAlerta.center = self.centroInicialPadrao
+                
+//                self.botaoAdicionaFarmacia.removeFromSuperview()
+//                self.botaoAdicionaRemedio.removeFromSuperview()
+//                self.botaoAdicionaAlerta.removeFromSuperview()
+                
+                }, completion: nil)
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -101,8 +170,12 @@ class RemediosViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
-        var tomei = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Tomei" , handler: {(action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in })
-        var apagar = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Apagar" , handler: {(action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in })
+        var tomei = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Tomei" , handler: {(action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            //Ação para quando o usuário tomou um remédio
+        })
+        var apagar = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Apagar" , handler: {(action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            //Ação para quando o usuário quer apagar um remédio
+        })
 
         tomei.backgroundColor = UIColor(red: CGFloat(3/255.0), green: CGFloat(144/255.0), blue: CGFloat(178/255.0), alpha: CGFloat(1))
         apagar.backgroundColor = UIColor(red: CGFloat(237/255.0), green: CGFloat(37/255.0), blue: CGFloat(73/255.0), alpha: CGFloat(1))
