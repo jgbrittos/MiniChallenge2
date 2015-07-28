fb5e6ccac246e5c3e22c2c8eda146256c5b72b7d
caiquepereira <caiquepereira@gmail.com>
2015-07-28 10:35:30 -0300

    Arquivos de farmácia crados

---
+++ MiniFarma/MiniFarma/Farmacia.swift
@@ -0,0 +1,13 @@
+//
+//  Farmacia.swift
+//  MiniFarma
+//
+//  Created by Caique de Paula Pereira on 28/07/15.
+//  Copyright (c) 2015 JGBrittoS. All rights reserved.
+//
+
+import UIKit
+
+class Farmacia: NSObject {
+   
+}
