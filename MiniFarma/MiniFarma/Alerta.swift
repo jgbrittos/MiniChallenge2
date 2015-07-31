f38bd4340b0f2575b67bf25d4f1b55a4788336e0
caiquepereira <caiquepereira@gmail.com>
2015-07-31 10:32:05 -0300

    Criação do Alerta

--- MiniFarma/MiniFarma/Alerta.swift
+++ MiniFarma/MiniFarma/Alerta.swift
@@ -10,4 +10,19 @@ import UIKit
 
 class Alerta: NSObject {
    
+    var idAlerta : Int = 0
+    var dataInicio: NSDate
+    var numeroDuracao: Int = 0
+    var unidadeDuracao: Int = 0
+    var ativo: Int = 0
+    var idIntervalo: Int = 0
+    var idRemedio: Int = 0
+    
+
+    override init() {
+        self.dataInicio = NSDate()
+    }
+    
+    
+    
 }
---
+++ MiniFarma/MiniFarma/AlertaDAO.swift
@@ -0,0 +1,13 @@
+//
+//  AlertaDAO.swift
+//  MiniFarma
+//
+//  Created by Caique de Paula Pereira on 31/07/15.
+//  Copyright (c) 2015 JGBrittoS. All rights reserved.
+//
+
+import UIKit
+
+class AlertaDAO: DAO {
+   
+}
