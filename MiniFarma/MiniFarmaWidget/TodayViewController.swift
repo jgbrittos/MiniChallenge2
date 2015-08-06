9c4117d3ad45ff48eb8e703ad6dcd8847d94f66f
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-08-06 12:25:37 -0300

    fazendo widget com tres botoes

---
+++ MiniFarma/MiniFarmaWidget/TodayViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TodayViewController.swift
+//  MiniFarmaWidget
+//
+//  Created by João Gabriel de Britto e Silva on 05/08/15.
+//  Copyright (c) 2015 JGBrittoS. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    var info = NSObject()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        self.info = NSUserDefaults(suiteName: "group.br.com.jgbrittos.MiniFarma")!
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        completionHandler(NCUpdateResult.NewData)
+    }
+
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsetsZero
+    }
+    
+    @IBAction func addFarmacia(sender: AnyObject) {
+        println("Farmacia")
+        self.info.setValue("Farmacia", forKey: "storyboard")
+        var url =  NSURL(string:"MiniFarmaTodayExtension://")
+        self.extensionContext?.openURL(url!, completionHandler:{(success: Bool) -> Void in })
+    }
+    
+    @IBAction func addRemedio(sender: AnyObject) {
+        println("Remedio")
+        self.info.setValue("Remedio", forKey: "storyboard")
+        var url =  NSURL(string:"MiniFarmaTodayExtension://")
+        self.extensionContext?.openURL(url!, completionHandler:{(success: Bool) -> Void in })
+    }
+
+    @IBAction func addAlerta(sender: AnyObject) {
+        println("Alerta")
+        self.info.setValue("Alerta", forKey: "storyboard")
+        var url =  NSURL(string:"MiniFarmaTodayExtension://")
+        self.extensionContext?.openURL(url!, completionHandler:{(success: Bool) -> Void in })
+    }
+    
+}
